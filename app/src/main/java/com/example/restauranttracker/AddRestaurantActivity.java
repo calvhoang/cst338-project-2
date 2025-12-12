44c4bf22b83e4ed6380f4c997476d861ed0ee308
Calvin Hoang <calvinhoangch@gmail.com>
2025-12-12 04:53:32 -0800

    create AddRestaurantActivity with add button onClick and get user input method

--- app/src/main/java/com/example/restauranttracker/AddRestaurantActivity.java
+++ app/src/main/java/com/example/restauranttracker/AddRestaurantActivity.java
@@ -1,27 +1,47 @@
 package com.example.restauranttracker;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.restauranttracker.databinding.ActivityAddRestaurantBinding;
 
-
 public class AddRestaurantActivity extends AppCompatActivity {
 
     ActivityAddRestaurantBinding binding;
+
+    String restaurantName = "";
+    String cuisine = "";
+    String city = "";
+    int rating = 0;
+    boolean visited = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAddRestaurantBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.addRestaurantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUserInput();
+            }
+        });
     }
 
-    static Intent addRestaurantActivityIntentFactory(Context context) {
-        Intent intent = new Intent(context, AddRestaurantActivity.class);
-        return intent;
+    private void getUserInput() {
+        restaurantName = binding.restaurantNameInput.getText().toString();
+        cuisine = binding.restaurantCuisineInput.getText().toString();
+        city = binding.restaurantCityInput.getText().toString();
+        try {
+            rating = Integer.parseInt(binding.restaurantRatingInput.getText().toString());
+        } catch (NumberFormatException e) {
+            Log.d(MainActivity.TAG, "Error reading value from Rating EditText");
+        }
+        visited = binding.restaurantVisitedCheckbox.isChecked();
     }
+
 }
