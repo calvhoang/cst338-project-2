2d3ec920c0c0731134ef34a265116f26c1a2a0cf
Calvin Hoang <calvinhoangch@gmail.com>
2025-12-14 18:45:39 -0800

    create ViewHolder and Adapter class

---
+++ app/src/main/java/com/example/restauranttracker/viewHolders/AppAdapter.java
@@ -0,0 +1,8 @@
+package com.example.restauranttracker.viewHolders;
+
+import androidx.recyclerview.widget.ListAdapter;
+
+import com.example.restauranttracker.Database.entities.Restaurant;
+
+public class AppAdapter extends ListAdapter<> {
+}
---
+++ app/src/main/java/com/example/restauranttracker/viewHolders/AppViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.restauranttracker.viewHolders;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AppViewHolder extends RecyclerView.ViewHolder {
+
+    public AppViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+}
