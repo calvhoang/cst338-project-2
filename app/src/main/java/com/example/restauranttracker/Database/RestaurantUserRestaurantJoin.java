1f6111f550fb112f9fac6ddbf814158b2c084147
Calvin Hoang <calvinhoangch@gmail.com>
2025-12-14 18:52:55 -0800

    create POJO for joined Restaurant and UserRestaurant tables

---
+++ app/src/main/java/com/example/restauranttracker/Database/RestaurantUserRestaurantJoin.java
@@ -0,0 +1,13 @@
+package com.example.restauranttracker.Database;
+
+import java.time.LocalDateTime;
+
+public class RestaurantUserRestaurantJoin {
+    public long restaurantId;
+    public String name;
+    public String cuisine;
+
+    public int rating;
+    public boolean visited;
+    public LocalDateTime date;
+}
