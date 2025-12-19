e2a041192fdb873054f4c6f235bd293e4c88475e
Jordan Perez <jperezherrera@csumb.edu>
2025-12-18 23:44:32 -0800

    added 1 test

--- app/src/androidTest/java/com/example/restauranttracker/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/restauranttracker/ExampleInstrumentedTest.java
@@ -1,12 +1,19 @@
 package com.example.restauranttracker;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
 
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -22,5 +29,22 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.restauranttracker", appContext.getPackageName());
+    }
+    @Rule
+    public IntentsTestRule<LoginActivity> intentsTestRule = new IntentsTestRule<>(LoginActivity.class);
+
+
+    @Test
+    public void navigateFromLogin_ToSignIn_ToMain() {
+        // Click the button to start SignUpActivity
+        onView(withId(R.id.registerButton)).perform(click());
+        // Verify an Intent was sent to start SignUpActivity
+        intended(toPackage(SignUpActivity.class.getPackageName()));
+
+        // Click the button to start to MainActivity
+        onView(withId(R.id.signUpButton)).perform(click());
+        // Verify an Intent was sent to start MainActivity
+        intended(toPackage(MainActivity.class.getPackageName()));
+
     }
 }
